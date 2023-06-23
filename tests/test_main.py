17ac464900bffeaa2ff9a6b85ecd63f79bbc9d9f
Mahmoud Elbasiouny <elbasiouny.mahmoud@gmail.com>
2023-06-22 21:26:43 -0400

    added pytest fixture and updated tests to use it

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,75 +1,81 @@
 from unittest.mock import Mock
 
 import pyglet
+import pytest
 
 from tempus_fugit_minecraft.main import Window
 
 
-window = Window()
+@pytest.fixture(scope="class")
+def window():
+    window = Window()
+    return window
 
 
 class TestPauseMenu:
-    def mock_pause(self):
+    @staticmethod
+    def mock_pause(window):
         if not window.paused:
             window.on_key_press(pyglet.window.key.ESCAPE, Mock())
 
-    def mock_resume(self):
+    @staticmethod
+    def mock_resume(window):
         if window.paused:
             window.on_key_press(pyglet.window.key.ESCAPE, Mock())
 
-    def test_on_key_press(self):
-        self.mock_pause()
+    def test_on_key_press(self, window):
+        self.mock_pause(window)
         assert window.paused
 
-        self.mock_resume()
+        self.mock_resume(window)
         assert not window.paused
 
-    def test_update(self):
-        self.mock_pause()
+    def test_update(self, window):
+        self.mock_pause(window)
         before = window.model.process_queue()
         window.update(0)
         after = window.model.process_queue()
 
         assert before == after
 
-    def test_on_resize(self):
+    def test_on_resize(self, window):
         RESIZE_WIDTH = RESIZE_HEIGHT = 100
 
-        self.mock_pause()
+        self.mock_pause(window)
         window.on_resize(RESIZE_WIDTH, RESIZE_HEIGHT)
         assert window.pause_label.x == RESIZE_WIDTH // 2
         assert window.pause_label.y == RESIZE_HEIGHT // 2
 
-        self.mock_resume()
+        self.mock_resume(window)
 
-    def test_pause_and_resume_game(self):
+    def test_pause_and_resume_game(self, window):
         window.pause_game()
         assert window.paused
-        assert window.exclusive is False
+        assert not window.exclusive
 
         window.resume_game()
         assert not window.paused
-        assert window.exclusive is True
+        assert window.exclusive
 
-    def test_draw_pause_menu(self):
-        assert pyglet.gl.glIsEnabled(pyglet.gl.GL_BLEND) != pyglet.gl.GL_TRUE
-        assert pyglet.gl.glIsEnabled(pyglet.gl.GL_DEPTH_TEST) != pyglet.gl.GL_TRUE
+    def test_draw_pause_menu(self, window):
+        assert not pyglet.gl.glIsEnabled(pyglet.gl.GL_BLEND)
+        assert not pyglet.gl.glIsEnabled(pyglet.gl.GL_DEPTH_TEST)
 
-        self.mock_pause()
+        self.mock_pause(window)
         window.draw_pause_menu()
-        assert pyglet.gl.glIsEnabled(pyglet.gl.GL_BLEND) == pyglet.gl.GL_TRUE
-        assert pyglet.gl.glIsEnabled(pyglet.gl.GL_DEPTH_TEST) == pyglet.gl.GL_TRUE
+        assert pyglet.gl.glIsEnabled(pyglet.gl.GL_BLEND)
+        assert pyglet.gl.glIsEnabled(pyglet.gl.GL_DEPTH_TEST)
 
-        self.mock_resume()
+        self.mock_resume(window)
 
-    def test_on_mouse_press(self):
-        self.mock_pause()
+    def test_on_mouse_press(self, window):
+        self.mock_pause(window)
         window.on_mouse_press(window.resume_label.x, window.resume_label.y, 1, 0)
-        assert window.paused is False
+        assert not window.paused
 
-        self.mock_resume()
+        self.mock_resume(window)
 
-    def test_within_label(self):
+    def test_within_label(self, window):
         test_label = pyglet.text.Label(
             text="Test Label",
             font_name="Arial",
@@ -82,13 +88,13 @@ class TestPauseMenu:
 
         x = window.width // 2
         y = window.height // 2
-        assert window.within_label(x, y, test_label) is True
+        assert window.within_label(x, y, test_label)
 
         x = window.width + 1
         y = window.height + 1
-        assert window.within_label(x, y, test_label) is False
+        assert not window.within_label(x, y, test_label)
 
-    def test_center_labels(self):
+    def test_center_labels(self, window):
         RESIZE_WIDTH = RESIZE_HEIGHT = 100
 
         window.on_resize(RESIZE_WIDTH, RESIZE_HEIGHT)
