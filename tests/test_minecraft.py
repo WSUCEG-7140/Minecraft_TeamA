dc46eade856a977ad1564ce624283c35ba5b6dbb
bakursait2 <sait.3@wright.edu>
2023-06-25 18:41:43 -0400

    add&update tests to fit Dark_cloud block; update test_center_labels() to succeed

--- tests/test_minecraft.py
+++ tests/test_minecraft.py
@@ -2,7 +2,7 @@ from unittest.mock import Mock
 
 import pyglet
 
-from tempus_fugit_minecraft.main import Model, Window, LIGHT_CLOUD
+from tempus_fugit_minecraft.main import Model, Window, LIGHT_CLOUD, DARK_CLOUD
 
 
 class TestSpeed:
@@ -31,30 +31,43 @@ class TestSpeed:
 class TestClouds:
     def test_light_clouds_created_dynamically(self):
         model = Model()
-        model.generate_clouds(80, 100)
+        model.generate_clouds_positions(80, 100)
         count_clouds = sum(1 for block in model.world.values() if block == LIGHT_CLOUD)
         
         assert  count_clouds >= 1, "LIGHT_CLOUD was not identified"
 
     def test_cloud_positions(self):
         model = Model()
-        model.generate_clouds(80, 100)
-        o = 80
-        cloud_blocks = [coord for coord, block in model.world.items() if block == LIGHT_CLOUD]
+        model.generate_clouds_positions(80, 100)
+        o = 80 + 2*6        # + 2*6 to insure that the test will cover cloud block outside the world.
+        cloud_blocks = [coord for coord, block in model.world.items() if block in [LIGHT_CLOUD,DARK_CLOUD]]
         for block in cloud_blocks:
             assert -o <= block[0] <= o
             assert -o <= block[2] <= o
 
     def test_cloud_height(self):
         model = Model()
-        model.generate_clouds(80, 100)
-        clouds = [coord for coord, block in model.world.items() if block == LIGHT_CLOUD]
+        model.generate_clouds_positions(80, 100)
+        clouds = [coord for coord, block in model.world.items() if block in [LIGHT_CLOUD,DARK_CLOUD]]
         for cloud_coordinates in clouds:
             assert cloud_coordinates[1] >= 20
 
     def test_non_overlapping_clouds(self):
         model = Model()
-        model.generate_clouds(80, 100)
-        blocks_of_all_clouds = [coordinates for coordinates , block in model.world.items() if block == LIGHT_CLOUD]
+        model.generate_clouds_positions(80, 100)
+        blocks_of_all_clouds = [coordinates for coordinates , block in model.world.items() if block in [LIGHT_CLOUD,DARK_CLOUD]]
         unique_clouds = set(blocks_of_all_clouds)
         assert len(blocks_of_all_clouds) == len(unique_clouds)
+    
+    def test_dark_clouds_created_dynamically(self):
+        model = Model()
+        model.generate_clouds_positions(80,200)
+        count_dark_cloud_blocks = sum(1 for block in model.world.values() if block == DARK_CLOUD)
+        assert count_dark_cloud_blocks >= 1, "DARK_CLOUDs were not identified"
+    
+    def test_draw_clouds_in_the_sky_and_count_blocks(self):
+        model = Model()
+        clouds = model.generate_clouds_positions(80,150)
+        model.draw_clouds_in_the_sky(clouds)
+        cloud_blocks = [coordinates for coordinates , block in model.world.items() if block in [LIGHT_CLOUD , DARK_CLOUD]]
+        assert len(cloud_blocks) >= sum(len(cloud) for cloud in clouds), "cloud blocks are not identified"
