af9fe06495633b12d2b8cb10e89792275eb57073
xieWright <xie.34@wright.edu>
2023-07-11 22:32:44 -0400

    Added the tests and started on implementing it into the window

--- tempus_fugit_minecraft/shaders.py
+++ tempus_fugit_minecraft/shaders.py
@@ -1,8 +1,19 @@
 from pyglet.gl import *
 import pyglet.graphics as graphics
+import cmath as math
 from tempus_fugit_minecraft.utilities import *
 
 class Shaders():
     def __init__(self, model):
         self.blockInformation = model._shown
+        '''Solves issue #7'''
+        def TurnOnEnvironmentLight():
+            glEnable(GL_LIGHTING)
+            glEnable(GL_LIGHT0)
+            lightpos = {0, -1, 0, 0.}
+            glLightfv(GL_LIGHT0, GL_POSITION, lightpos)
+        
+        def Normal3DVectorCalc(vector):
+            ThreeDVector = math.sqrt(vector[0] * v[0] + vector[1] * vector[1] + vector[2] * vector[2])
+            return ThreeDVector
         
--- tests/test_lighting.py
+++ tests/test_lighting.py
@@ -1,3 +1,18 @@
 import pyglet
 import pytest
+from pyglet.gl import *
+from unittest.mock import Mock
+from tempus_fugit_minecraft.window import Window
+
+@pytest.fixture(scope="class")
+def window():
+    yield Window()
+
+class TestLighting():
+    def test_lights_turned_on(self, window):
+        assert glIsEnabled(GL_LIGHTING)
+        assert glIsEnabled(GL_LIGHT0)
+    
+    def test_3D_norm_vector_calc():
+        
 
