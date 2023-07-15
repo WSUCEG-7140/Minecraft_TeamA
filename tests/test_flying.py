916525a89637cc02a3448f52d81b993d13abf429
Mahmoud Elbasiouny <elbasiouny.mahmoud@gmail.com>
2023-07-15 18:12:25 -0400

    added tests for ascend and descend

---
+++ tests/test_flying.py
@@ -0,0 +1,35 @@
+from unittest.mock import Mock
+
+import pyglet
+import pytest
+
+from tempus_fugit_minecraft.utilities import (
+    BRICK,
+    DARK_CLOUD,
+    GRASS,
+    LIGHT_CLOUD,
+    SAND,
+    STONE,
+)
+from tempus_fugit_minecraft.window import Window
+
+
+@pytest.fixture(scope="class")
+def window():
+    yield Window()
+
+
+class TestFlying:
+    @pytest.fixture(autouse=True)
+    def teardown(self, window):
+        window.flying = False
+
+    def test_ascend(self, window):
+        window.on_key_press(pyglet.window.key.TAB, Mock())
+        window.on_key_press(pyglet.window.key.SPACE, Mock())
+        assert window.ascend is True
+
+    def test_descend(self, window):
+        window.on_key_press(pyglet.window.key.TAB, Mock())
+        window.on_key_press(pyglet.window.key.LSHIFT, Mock())
+        assert window.descend is True
