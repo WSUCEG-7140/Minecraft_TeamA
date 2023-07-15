19d243cbfc0742b34f57812e5b37253a0c1d62b8
xieWright <xie.34@wright.edu>
2023-07-15 14:24:58 -0400

    pause changed so it doesn't get affected by shaders

--- tempus_fugit_minecraft/shaders.py
+++ tempus_fugit_minecraft/shaders.py
@@ -10,9 +10,9 @@ def to_cfloat(vector):
 class Shaders():
     def __init__(self, model):
         self.blockInformation = model._shown
-        self.ambient = to_cfloat([5, 5, 5])
-        self.diffuse = to_cfloat([5, 5, 5])
-        self.specular = to_cfloat([5, 5, 5])
+        self.ambient = to_cfloat([3, 3, 3])
+        self.diffuse = to_cfloat([3, 3, 3])
+        self.specular = to_cfloat([3, 3, 3])
         '''Solves issue #7'''
     def turn_on_environment_light(self):
         glEnable(GL_LIGHTING)
@@ -23,6 +23,7 @@ class Shaders():
         glLightfv(GL_LIGHT0, GL_AMBIENT, self.ambient)
         glLightfv(GL_LIGHT0, GL_DIFFUSE, self.diffuse)
         glLightfv(GL_LIGHT0, GL_SPECULAR, self.specular)
+        return 1
         
     @staticmethod
     def normal_3D_vector_calc(vector):
