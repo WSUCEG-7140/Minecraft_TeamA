a6fb2647d27bcf157201f5bd7e4c901af982d292
bakursait2 <sait.3@wright.edu>
2023-07-16 12:33:41 -0400

    update TestBlock: test the tree components (2 blocks).

--- tests/test_block.py
+++ tests/test_block.py
@@ -2,7 +2,7 @@ from tempus_fugit_minecraft.block import *
 
 class TestBlock:
     def test_specified_blocks_are_breakable(self):
-        blocks = [GRASS, SAND, BRICK]
+        blocks = [GRASS, SAND, BRICK, TREE_LEAVES, TREE_TRUNK]
         
         for block in blocks: 
             assert block.is_breakable
@@ -14,19 +14,19 @@ class TestBlock:
             assert not block.is_breakable
 
     def test_specified_blocks_are_collidable(self):
-        blocks = [GRASS, SAND, BRICK, STONE]
+        blocks = [GRASS, SAND, BRICK, STONE,TREE_TRUNK]
         
         for block in blocks: 
             assert block.is_collidable
     
     def test_specified_blocks_not_collidable(self):
-        blocks = [LIGHT_CLOUD, DARK_CLOUD]
+        blocks = [LIGHT_CLOUD, DARK_CLOUD, TREE_LEAVES]
 
         for block in blocks:
             assert not block.is_collidable
 
     def test_specified_blocks_placeable(self):
-        blocks = [GRASS, SAND, BRICK, STONE]
+        blocks = [GRASS, SAND, BRICK, TREE_LEAVES, TREE_TRUNK]
         
         for block in blocks:
             assert block.can_build_on
