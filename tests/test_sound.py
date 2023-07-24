27b2e1545c01f89f7602ba9a90ccd14d6ddfecd0
xieWright <xie.34@wright.edu>
2023-07-24 19:00:06 -0400

    added test for adjust_all_volume

--- tests/test_sound.py
+++ tests/test_sound.py
@@ -29,3 +29,20 @@ class TestSoundList:
     def test_add_sound_to_dictionary(self, sound_list):
         sound_list.add_sound_to_dictionary('rock_hit', Sound(parent_directory + "/assets/sound/rock_hit.wav"))
         assert sound_list.dictionary['rock_hit']
+        assert not sound_list.add_sound_to_dictionary('rock_hit', Sound(parent_directory + "/assets/sound/rock_hit.wav"))
+    
+    def test_adjust_all_volume(self, sound_list):
+        sound_list.add_sound_to_dictionary('rock_hit1', Sound(parent_directory + "/assets/sound/rock_hit.wav"))
+        sound_list.add_sound_to_dictionary('rock_hit2', Sound(parent_directory + "/assets/sound/rock_hit.wav"))
+        sound_list.add_sound_to_dictionary('rock_hit3', Sound(parent_directory + "/assets/sound/rock_hit.wav"))
+        rock_hit_1_volume = sound_list.dictionary['rock_hit1'].volume
+        rock_hit_2_volume = sound_list.dictionary['rock_hit2'].volume
+        rock_hit_3_volume = sound_list.dictionary['rock_hit3'].volume
+        sound_list.adjust_all_volume(.1)
+        assert rock_hit_1_volume != sound_list.dictionary['rock_hit1'].volume
+        assert rock_hit_2_volume != sound_list.dictionary['rock_hit2'].volume
+        assert rock_hit_3_volume != sound_list.dictionary['rock_hit3'].volume
+        sound_list.adjust_all_volume(-1)
+        assert sound_list.dictionary['rock_hit1'].volume == 0
+        sound_list.adjust_all_volume(2)
+        assert sound_list.dictionary['rock_hit1'].volume == 1
