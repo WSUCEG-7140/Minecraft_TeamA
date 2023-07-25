35f9e894005677c222c74de02b1261bb7ddb49d5
Mohammad Shalash <shalash.21@wright.edu>
2023-07-25 02:56:11 -0400

    issue#33 issue#34 players can build clouds Test

--- tests/test_block.py
+++ tests/test_block.py
@@ -2,16 +2,16 @@ from tempus_fugit_minecraft.block import *
 
 
 class TestBlock:
-    #issue47; #issue80
+    # issue#47; #issue#80; issue#33; issue#34 
     def test_specified_blocks_are_breakable(self):
-        blocks = [GRASS, SAND, BRICK, TREE_LEAVES, TREE_TRUNK]
+        blocks = [GRASS, SAND, BRICK, TREE_LEAVES, TREE_TRUNK, LIGHT_CLOUD, DARK_CLOUD]
 
         for block in blocks:
             assert block.is_breakable
 
-    #issue47
+     # issue#47; #issue#80; issue#33; issue#34 
     def test_specified_blocks_not_breakable(self):
-        blocks = [STONE, LIGHT_CLOUD, DARK_CLOUD]
+        blocks = [STONE]
 
         for block in blocks:
             assert not block.is_breakable
@@ -32,14 +32,15 @@ class TestBlock:
 
     #issue47; #issue80
     def test_specified_blocks_placeable(self):
-        blocks = [GRASS, SAND, BRICK, TREE_LEAVES, TREE_TRUNK]
+        blocks = [GRASS, SAND, BRICK, TREE_LEAVES, TREE_TRUNK, LIGHT_CLOUD, DARK_CLOUD]
 
         for block in blocks:
             assert block.can_build_on
 
     #issue47
-    def test_specified_blocks_not_placeable(self):
+    """1def test_specified_blocks_not_placeable(self):
         blocks = [LIGHT_CLOUD, DARK_CLOUD]
 
         for block in blocks:
             assert not block.can_build_on
+    """
