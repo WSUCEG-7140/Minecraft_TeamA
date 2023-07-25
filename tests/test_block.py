880433b4779a9427c5b97bd10009885cafa5d0d1
Mohammad Shalash <shalash.21@wright.edu>
2023-07-25 18:54:17 -0400

    issue#33 Player build light clouds&others anywhere

--- tests/test_block.py
+++ tests/test_block.py
@@ -2,44 +2,44 @@ from tempus_fugit_minecraft.block import *
 
 
 class TestBlock:
-    # issue#47 #issue#80
+    # issue#47 issue#80 issue#33 
     def test_specified_blocks_are_breakable(self):
-        blocks = [GRASS, SAND, BRICK, TREE_LEAVES, TREE_TRUNK]
+        blocks = [GRASS, SAND, BRICK, TREE_LEAVES, TREE_TRUNK, LIGHT_CLOUD]
 
         for block in blocks:
             assert block.is_breakable
 
-     # issue#47 #issue#80 
+    # issue#47 #issue#80 issue#33
     def test_specified_blocks_not_breakable(self):
-        blocks = [STONE, LIGHT_CLOUD, DARK_CLOUD]
+        blocks = [STONE, DARK_CLOUD]
 
         for block in blocks:
             assert not block.is_breakable
 
-    #issue47; #issue80
+    #issue47 #issue80 issue#33
     def test_specified_blocks_are_collidable(self):
         blocks = [GRASS, SAND, BRICK, STONE,TREE_TRUNK]
 
         for block in blocks:
             assert block.is_collidable
 
-    #issue47; #issue80
+    #issue47 #issue80 issue#33
     def test_specified_blocks_not_collidable(self):
         blocks = [LIGHT_CLOUD, DARK_CLOUD, TREE_LEAVES]
 
         for block in blocks:
             assert not block.is_collidable
 
-    #issue47; #issue80
+    #issue47; #issue80 #issue33
     def test_specified_blocks_placeable(self):
-        blocks = [GRASS, SAND, BRICK, TREE_LEAVES, TREE_TRUNK]
+        blocks = [GRASS, SAND, BRICK, TREE_LEAVES, TREE_TRUNK, LIGHT_CLOUD]
 
         for block in blocks:
             assert block.can_build_on
 
-    #issue47
+    #issue47 #issue33
     def test_specified_blocks_not_placeable(self):
-        blocks = [LIGHT_CLOUD, DARK_CLOUD]
+        blocks = [DARK_CLOUD]
 
         for block in blocks:
             assert not block.can_build_on
